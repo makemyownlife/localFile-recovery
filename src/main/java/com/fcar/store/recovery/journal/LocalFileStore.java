56273d78d6f0fc7962676a8e6fe369ab3b28c77b
zhangyong04 <zhangyong7120180@163.com>
2017-04-10 18:15:28 +0800

    本地 文件 的相关 111

--- src/main/java/com/fcar/store/recovery/journal/LocalFileStore.java
+++ src/main/java/com/fcar/store/recovery/journal/LocalFileStore.java
@@ -4,8 +4,10 @@ import com.fcar.store.recovery.AbstractStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * 本地文件日志存储
@@ -38,6 +40,7 @@ public class LocalFileStore implements AbstractStore {
         } else {
             this.indexMap = indexMap;
         }
+
         this.initLoad();
     }
 
@@ -46,8 +49,57 @@ public class LocalFileStore implements AbstractStore {
      */
     private void initLoad() throws IOException {
         logger.warn("开始恢复数据");
+        final String nm = this.name + ".";
+        final File parentDir = new File(this.path);
+        this.checkParentDir(parentDir);
+        //所有的数据文件 不包含操作日志文件
+        final File[] dataFiles = parentDir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(final File dir, final String n) {
+                return n.startsWith(nm) && !n.endsWith(".log");
+            }
+        });
+        if (dataFiles == null || dataFiles.length == 0) {
+            logger.warn("父目录:{} ，没有查找到名为：{}的数据文件!", this.path, this.name);
+            return;
+        }
+        logger.warn("遍历每个数据文件");
+        //提取出每个数据文件的文件号,然后排序分开处理
+        final List<Integer> indexList = new LinkedList<Integer>();
+        for (final File f : dataFiles) {
+            try {
+                final String fn = f.getName();
+                final int n = Integer.parseInt(fn.substring(nm.length()));
+                indexList.add(Integer.valueOf(n));
+            } catch (final Exception e) {
+                logger.error("parse file index error" + f, e);
+            }
+        }
+        Integer[] indices = indexList.toArray(new Integer[indexList.size()]);
+        //对文件顺序进行排序(从小到大排序)
+        Arrays.sort(indices);
+        for (final Integer n : indices) {
+            logger.warn("处理index为" + n + "的文件");
+            Map<BytesKey, OperateItem> tempIndexMap = new HashMap<BytesKey, OperateItem>();
+            //数据文件
+            File file = new File(parentDir, this.name + "." + n);
+            //数据文件
+            LocalFile dataLocalFile = new LocalFile(file, n, this.force);
+            //操作记录文件
+            LogLocalFile logLocalFile = new LogLocalFile(new File(file.getAbsolutePath() + ".log"), n, this.force);
+            //总的操作数
+            long itemsCount = dataLocalFile.length() / OperateItem.LENGTH;
 
+        }
+    }
 
+    /**
+     * Create the parent directory if it doesn't exist.
+     */
+    private void checkParentDir(final File parent) {
+        if (!parent.exists() && !parent.mkdirs()) {
+            throw new IllegalStateException("Can't make dir " + this.path);
+        }
     }
 
     @Override
