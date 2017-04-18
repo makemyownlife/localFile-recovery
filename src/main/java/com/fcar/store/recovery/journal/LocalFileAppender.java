2ea5f68a9f35aed325d4ace8212daf7424abdaf2
zhangyong <zhangyong7120180@163.com>
2017-04-18 22:28:40 +0800

    添加appender 线程

--- src/main/java/com/fcar/store/recovery/journal/LocalFileAppender.java
+++ src/main/java/com/fcar/store/recovery/journal/LocalFileAppender.java
@@ -13,12 +13,41 @@ public class LocalFileAppender {
 
     private final static Logger logger = LoggerFactory.getLogger(LocalFileAppender.class);
 
-    private boolean started = false;
+    private volatile boolean started = false;
 
     private LocalFileStore localFileStore;
 
+    private Thread appendThread;
+
     public LocalFileAppender(LocalFileStore localFileStore) {
         this.localFileStore = localFileStore;
+        //启动异步入盘线程
+        this.startAppendThread();
+    }
+
+    private void startAppendThread() {
+        try {
+            this.appendThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    LocalFileAppender.this.processQueue();
+                }
+            });
+            logger.warn("start appendThread!");
+            this.appendThread.setName("appendThread");
+            this.appendThread.setPriority(Thread.MAX_PRIORITY);
+            this.appendThread.setDaemon(true);
+            this.appendThread.start();
+            this.started = true;
+        } catch (Throwable e) {
+            logger.error("startAppendThread error: ", e);
+        }
+    }
+
+    private void processQueue() {
+        while (true) {
+
+        }
     }
 
     public OperateItem store(byte operate, BytesKey bytesKey, final byte[] data, final boolean sync) throws IOException {
--- src/test/java/com/fcar/store/recovery/LocalFileStoreUnitTest.java
+++ src/test/java/com/fcar/store/recovery/LocalFileStoreUnitTest.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * Created by zhangyong on 2017/4/11.
@@ -15,7 +16,7 @@ public class LocalFileStoreUnitTest {
     private final static Logger logger = LoggerFactory.getLogger(LocalFileStoreUnitTest.class);
 
     @Test
-    public void testAddData() throws IOException {
+    public void testAddData() throws IOException, InterruptedException {
         String path = "D://localstore";
         String name = "order-store";
         AbstractStore store = new LocalFileStore(
@@ -24,6 +25,13 @@ public class LocalFileStoreUnitTest {
                 true,
                 null
         );
+        Long orderId = 87665L;
+        byte[] data = "张勇".getBytes("UTF-8");
+        final ByteBuffer buf = ByteBuffer.allocate(16);
+        buf.putLong(orderId);
+        byte[] arr = buf.array();
+        store.add(arr, data);
+        Thread.currentThread().sleep(500000);
     }
 
 }
