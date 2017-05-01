a49e4e2e3512eab0e13e61109a9b58c6de7da79e
zhangyong <zhangyong7120180@163.com>
2017-05-01 15:00:38 +0800

    提供公共的方法 appendMessage

--- src/main/java/com/fcar/store/recovery/RecoveryManager.java
+++ src/main/java/com/fcar/store/recovery/RecoveryManager.java
@@ -1,5 +1,6 @@
 package com.fcar.store.recovery;
 
+import com.fcar.store.recovery.journal.LocalFileStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,5 +11,31 @@ public class RecoveryManager {
 
     private final static Logger logger = LoggerFactory.getLogger(RecoveryManager.class);
 
+    private RecoveryConfig recoveryConfig;
+
+    private String name;
+
+    private String path;
+
+    private LocalFileStore localFileStore;
+
+    public RecoveryManager(RecoveryConfig recoveryConfig, String name, String path) {
+        this.recoveryConfig = recoveryConfig;
+        this.name = name;
+        this.path = path;
+        this.makeStore();
+    }
+
+    private void makeStore() {
+        try {
+            this.localFileStore = new LocalFileStore(this.path, this.name, true);
+        } catch (Exception e) {
+            logger.error("makeStore error: ", e);
+        }
+    }
+
+    public void appendMessage(Long key, byte[] data) {
+
+    }
 
 }
